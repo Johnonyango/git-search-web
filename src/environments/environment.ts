7b98a22b05ec1646b6ab4081375e43b9878b9604
Johnonyango <j.yayah7@gmail.com>
2019-07-15 11:08:50 +0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiUrl: "6d11b6cd9e6f91316808d1d897bbb842a1ec1f6f",
+};
+
+
